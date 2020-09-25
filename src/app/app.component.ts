1d21f23ae8af43af44dc9ad6c4df90d9a916d202
Julio Rodrigues <juulhao@gmail.com>
2020-09-24 22:30:00 -0300

    subindo..

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,25 +3,54 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { DomSanitizer } from '@angular/platform-browser';
+import { BluetoothSerial } from '@ionic-native/bluetooth-serial/ngx';
+
+import {
+  InAppBrowser,
+  InAppBrowserOptions,
+} from '@ionic-native/in-app-browser/ngx';
+import { PrintService } from './print.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  url: any;
+  bluetoothList: any = [];
+  selectedPrinter: any;
   constructor(
     private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private theInAppBrowser: InAppBrowser,
+    private sanitize: DomSanitizer,
+    public btSerial: BluetoothSerial,
+    private print: PrintService
   ) {
     this.initializeApp();
+
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
-      this.splashScreen.hide();
     });
+
+    this.selectedPrinter = this.print.connectToBluetoothPrinter();
+  }
+
+
+  urlpaste() {
+    this.url = 'https://cors-anywhere.herokuapp.com/http://www.uol.com.br';
+    return this.sanitize.bypassSecurityTrustResourceUrl(this.url);
+  }
+
+  // This will print
+  printStuff() {
+    // The text that you want to print
+    const myText = 'Está é uma impressão de teste \n\n\n Impressão teste :) \n\n\n';
+    this.print.sendToBluetoothPrinter(myText);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,12 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { BluetoothSerial } from '@ionic-native/bluetooth-serial/ngx';
+import { PrintService } from './print.service';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,6 +19,9 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
+    PrintService,
+    InAppBrowser,
+    BluetoothSerial,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
